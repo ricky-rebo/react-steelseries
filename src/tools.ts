0a3f6ff21713fdb837c6e8a61e3f804a383d4977
Riccardo Rebottini <ricky@rebottini.it>
2021-09-27 11:51:06 +0200

    add updateIfChanged tool for code improvement

--- src/tools.ts
+++ src/tools.ts
@@ -1,4 +1,12 @@
-
 export function definedAndChanged<T = unknown>(prop: T, prev: T) {
 	return (prop !== undefined && prop !== prev)
+}
+
+export function updateIfChanged<T = unknown>(prop: T, prev: T, updateFunction: (p?: T) => unknown) {
+	if(definedAndChanged(prop, prev)) {
+		updateFunction(prop);
+		return true;
+	}
+
+	return false;
 }
