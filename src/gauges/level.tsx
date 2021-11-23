eadf4c1ba3c037a9d33a954fbd4010a05bd58e38
Riccardo Rebottini <ricky@rebottini.it>
2021-11-23 12:48:26 +0100

    refactor Level gauge

--- src/gauges/level.tsx
+++ src/gauges/level.tsx
@@ -1,82 +1,70 @@
-import React from "react";
 import { Level as ssLevel, LevelParams } from "steelseries";
-import { definedAndChanged, updateIfChanged } from "../tools";
+
+import GaugeComponent from "./gauge-component";
 
 
 interface Props extends LevelParams {
 	size: number;
 	value?: number;
+	
 	animate?: boolean;
 	animationCallback?: ()=> void;
 }
 
 
-export class Level extends React.Component<Props> {
-	canvasRef: React.RefObject<HTMLCanvasElement>;
-	gauge: ssLevel;
+export class Level extends GaugeComponent<Props, ssLevel, LevelParams> {
+	GaugeClass = ssLevel;
+	ignoredProps = ["animate", "animationCallback"];
 
-	constructor(props: Props) {
-		super(props);
-		this.canvasRef = React.createRef();
-	}
+	getGaugeParams = () => ({
+		size: this.props.size,
+		frameDesign: this.props.frameDesign,
+		frameVisible: this.props.frameVisible,
+		backgroundColor: this.props.backgroundColor,
+		backgroundVisible: this.props.backgroundVisible,
+		foregroundType: this.props.foregroundType,
+		foregroundVisible: this.props.foregroundVisible,
+		pointerColor: this.props.pointerColor,
+		decimalsVisible: this.props.decimalsVisible,
+		textOrientationFixed: this.props.textOrientationFixed,
+		rotateFace: this.props.rotateFace,
+	});
 
-	componentDidMount() {
-		if(this.canvasRef.current) {
-			this.gauge = new ssLevel(this.canvasRef.current, {
-				size: this.props.size,
-				frameDesign: this.props.frameDesign,
-				frameVisible: this.props.frameVisible,
-				backgroundColor: this.props.backgroundColor,
-				backgroundVisible: this.props.backgroundVisible,
-				foregroundType: this.props.foregroundType,
-				foregroundVisible: this.props.foregroundVisible,
-				pointerColor: this.props.pointerColor,
-				decimalsVisible: this.props.decimalsVisible,
-				textOrientationFixed: this.props.textOrientationFixed,
-				rotateFace: this.props.rotateFace,
-			});
-		
-			if(this.props.value !== undefined) {
-				this.props.animate
-				? this.gauge.setValueAnimated(this.props.value, this.props.animationCallback)
-				: this.gauge.setValue(this.props.value);
-			}
+	gaugePostInit(animate: boolean) {
+		if(this.props.value !== undefined) {
+			this.props.animate && animate
+			? this.gauge.setValueAnimated(this.props.value, this.props.animationCallback)
+			: this.gauge.setValue(this.props.value);
 		}
 	}
 
-	gaugeShouldRepaint(prev: Props) {
-		return (this.props.size !== prev.size)
-			|| definedAndChanged(this.props.frameVisible, prev.frameVisible)
-			|| definedAndChanged(this.props.backgroundVisible, prev.backgroundVisible)
-			|| definedAndChanged(this.props.foregroundVisible, prev.foregroundVisible)
-			|| definedAndChanged(this.props.decimalsVisible, prev.decimalsVisible)
-			|| definedAndChanged(this.props.textOrientationFixed, prev.textOrientationFixed)
-			|| definedAndChanged(this.props.rotateFace, prev.rotateFace);
+	setFrameDesign() {
+		this.log(`set frameDesign`);
+		this.gauge.setFrameDesign(this.props.frameDesign);
 	}
 
-	componentDidUpdate(prev: Props) {
-		if(this.canvasRef.current) {
-			if(this.gaugeShouldRepaint(prev)) {
-				this.componentDidMount();
-			}
-			else {
-				const { props } = this;
+	setBackgroundColor() {
+		this.log(`set backgroundColor`)
+		this.gauge.setBackgroundColor(this.props.backgroundColor);
+	}
 
-				updateIfChanged(props.frameDesign, prev.frameDesign, this.gauge.setFrameDesign.bind(this.gauge));
-				updateIfChanged(props.backgroundColor, prev.backgroundColor, this.gauge.setBackgroundColor.bind(this.gauge));
-				updateIfChanged(props.foregroundType, prev.foregroundType, this.gauge.setForegroundType.bind(this.gauge));
-				updateIfChanged(props.pointerColor, prev.pointerColor, this.gauge.setPointerColor.bind(this.gauge));
+	setForegroundType() {
+		this.log(`set foregrundType`);
+		this.gauge.setForegroundType(this.props.foregroundType);
+	}
 
-				updateIfChanged(props.value, prev.value, () => {
-					props.animate
-						? this.gauge.setValueAnimated(props.value, props.animationCallback)
-						: this.gauge.setValue(props.value);
-				});
-			}
+	setPointerColor() {
+		this.log("set pointerColor");
+		this.gauge.setPointerColor(this.props.pointerColor);
+	}
+
+	setValue() {
+		this.log(`set value`);
+		if(this.props.animate) {
+			this.gauge.setValueAnimated(this.props.value, this.props.animationCallback);
 		}
-	}
-
-	render() {
-		return <canvas ref={this.canvasRef}></canvas>
+		else {
+			this.gauge.setValue(this.props.value);
+		}
 	}
 }
