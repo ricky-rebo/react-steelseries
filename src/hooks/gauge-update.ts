62bef065ff9f94156899c265af27a8daa4886228
Riccardo Rebottini <ricky@rebottini.it>
2022-04-24 20:07:55 +0200

    :sparkles: add custom hook for animated gauge value update

--- src/hooks/gauge-update.ts
+++ src/hooks/gauge-update.ts
@@ -7,22 +7,42 @@ type Getter = () => unknown
 type Setter<G, V> = (value: V) => G | void
 type SetterWithCallback<G, V> = (value: V, callback: () => {}) => G | void
 
-/**
- * Base Gauge with specific setter + generic setters and getters
- */
-type BG<G, SN extends keyof G, V, _OMNs extends Exclude<keyof G, SN> = Exclude<keyof G, SN>> = {
+/** Base Gauge with specific setter + generic setters and getters */
+type Gauge<G, SN extends keyof G, V> = {
 	[setterName in SN]: Setter<G, V>
 } & {
-	[methodName in _OMNs]: Getter | Setter<G, unknown> | SetterWithCallback<G, unknown>
+	[methodName in Exclude<keyof G, SN>]: Getter | Setter<G, unknown> | SetterWithCallback<G, unknown>
 }
 
-export function useInitUpdateGaugeProp<V, G extends BG<G, SN, V>, SN extends keyof G> (
+/** Base gauge with setValue and setValueAnimated methods + generic setters and getters */
+type AnimatableGauge<G, V> = {
+	setValue: Setter<G, V>
+	setValueAnimated: SetterWithCallback<G, V>
+} & {
+	[methodName in Exclude<keyof G, "setValue" | "setValueAnimated">]: Getter | Setter<G, unknown> | SetterWithCallback<G, unknown>
+}
+
+export function useSetGaugeProp<V, G extends Gauge<G, SN, V>, SN extends keyof G> (
 	gaugeRef: Ref<G>, setterName: SN, value: V
 ) {
 	useEffect(() => { gaugeRef.current && gaugeRef.current[setterName](value) }, [value])
 }
 
-export function useUpdateGaugeProp<V, G extends BG<G, SN, V>, SN extends keyof G> (
+export function useSetGaugeValue<V, G extends AnimatableGauge<G, V>> (
+	gaugeRef: Ref<G>, value: V, animate: boolean, callback: () => {}
+) {
+	useEffect(() => {
+		if (gaugeRef.current) {
+			if (animate) {
+				gaugeRef.current.setValueAnimated(value, callback)
+			} else {
+				gaugeRef.current.setValue(value)
+			}
+		}
+	}, [value])
+}
+
+export function useUpdateGaugeProp<V, G extends Gauge<G, SN, V>, SN extends keyof G> (
 	gaugeRef: Ref<G>, setterName: SN, value: V
 ) {
 	useDidUpdate(() => { gaugeRef.current && gaugeRef.current[setterName](value) }, [value])
